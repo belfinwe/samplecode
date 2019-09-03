e771328a37759d9a8b569875258877c0f521975c
Joakim Nilsen <joakimrn02@gmail.com>
2019-09-03 15:23:59 +0200

    Inital commit, added php_databaseConnector.php

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # samplecode
+
 Just some sample codes for different scenarios
+
+__Naming conventions:__  
+`nameOfLanguage_shortDescription.relevantExtention`
---
+++ php_databaseConnector.php
@@ -0,0 +1,16 @@
+class myPDO extends PDO {
+	public function __construct() {
+	  $settings = parse_ini_file('file/db_info.ini',TRUE);
+	  if (!$settings) throw new exception('Can not open ini file.');
+	  $drv = $settings['database']['driver'];
+	  $hst = $settings['database']['host'];
+	  $sch = $settings['database']['schema'];
+	  $usr = $settings['database']['username'];
+	  $pwd = $settings['database']['password'];
+	  $dns = $drv . ':host=' . $hst . ';dbname=' . $sch;
+	  $options = array(
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	  );
+	  parent::__construct($dns,$usr,$pwd,$options);
+	}
+}
